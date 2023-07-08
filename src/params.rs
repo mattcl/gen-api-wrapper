54a835e589bd3dbc9ea2c4f8442b1426dcaac5cf
Matt Chun-Lum <matt@questionable.engineering>
2023-07-08 13:23:41 -0700

    chore: add back in license comments (main license ref still at root)

--- src/params.rs
+++ src/params.rs
@@ -1,11 +1,15 @@
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+//
 // Originally from https://gitlab.kitware.com/utils/rust-gitlab
 //
 // Modified in an attempt to make it general beyond just gitlab
+//
 
 //! Endpoint prelude
-//!
-//! This module re-exports all of the types needed for endpoints to implement the
-//! [`Endpoint`](../trait.Endpoint.html) trait.
 
 use std::borrow::Cow;
 
--- src/query.rs
+++ src/query.rs
@@ -1,6 +1,13 @@
+// // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+//
 // Originally from https://gitlab.kitware.com/utils/rust-gitlab
 //
 // Modified in an attempt to make it general beyond just gitlab
+//
 
 use async_trait::async_trait;
 use http::Uri;
